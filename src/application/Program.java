b1ada4834c41550b8c0be1ec786f2808306c6283
Aline Jamberci <jambercialine@outlook.com>
2023-08-16 13:02:02 -0300

    Exercicio de introdução a POO

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,37 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entites.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		Employee emp = new Employee ();
+		
+		System.out.print("Nome: ");
+		emp.nome = sc.nextLine();
+		System.out.print("Salário bruto: ");
+		emp.salarioBruto = sc.nextDouble();
+		System.out.print("Taxa: ");
+		emp.taxa = sc.nextDouble();
+		
+		System.out.println("Funcionário" +  emp);
+		
+		System.out.println();
+		System.out.print(" Entre com uma porcentagem: ");
+		double porcentagem = sc.nextDouble();
+		emp.incrementoSalario(porcentagem);
+		
+		System.out.println();
+		System.out.println("Atualização dos dados: " + emp);
+		
+		sc.close();
 
 	}
 
--- src/entites/Employee.java
+++ src/entites/Employee.java
@@ -1,5 +1,20 @@
 package entites;
 
 public class Employee {
-
+	
+	public String nome;
+	public double salarioBruto;
+	public double taxa;
+	
+	public double salarioLiquido () {
+		return salarioBruto - taxa;
+	}	
+	
+	public void incrementoSalario (double porcentagem) {
+		 salarioBruto += salarioBruto * porcentagem/100;
+	}
+	
+	public String toString () {
+		return nome + ", $" + String.format("%.2f", salarioLiquido());
+	}
 }
